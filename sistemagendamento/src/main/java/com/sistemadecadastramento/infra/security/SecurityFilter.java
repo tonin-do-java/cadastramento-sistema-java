af51b725ee5d86cbaf662bb015ae6cb44945f169
tonin-do-java <ant.fds.com@gmail.com>
2026-06-04 20:29:50 -0300

    feat(auth): criar filtro de segurança

---
+++ sistemagendamento/src/main/java/com/sistemadecadastramento/infra/security/SecurityFilter.java
@@ -0,0 +1,58 @@
+package com.sistemadecadastramento.infra.security;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter{
+
+    private final TokenService tokenService;
+    private final AutenticacaoService autenticacaoService;
+    
+    
+    public SecurityFilter(TokenService tokenService, AutenticacaoService autenticacaoService) {
+        this.tokenService = tokenService;
+        this.autenticacaoService = autenticacaoService;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+                
+                var token = recuperarToken(request);
+
+                if(token != null){
+                    var subject = tokenService.validarToken(token);
+
+                    if(subject != null){
+                        UserDetails usuario = autenticacaoService.loadUserByUsername(subject);
+
+                        var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                    }
+                }
+                filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request){
+        var authorizationHeader = request.getHeader("Autorizado");
+        if (authorizationHeader != null){
+            return authorizationHeader.replace("Bearer ", " ").trim();
+        }
+        return null;
+    }
+    
+}
