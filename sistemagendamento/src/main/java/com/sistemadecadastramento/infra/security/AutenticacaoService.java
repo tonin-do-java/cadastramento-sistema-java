dadd38f013cf1e5b4af8773585456a01b7abd8a5
tonin-do-java <ant.fds.com@gmail.com>
2026-06-04 20:20:47 -0300

    fix(auth): colocar a anotação correta

--- sistemagendamento/src/main/java/com/sistemadecadastramento/infra/security/AutenticacaoService.java
+++ sistemagendamento/src/main/java/com/sistemadecadastramento/infra/security/AutenticacaoService.java
@@ -3,11 +3,11 @@ package com.sistemadecadastramento.infra.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-
-
+import org.springframework.stereotype.Service;
 
 import com.sistemadecadastramento.services.UsuarioService;
 
+@Service
 public class AutenticacaoService implements UserDetailsService {
 
     @Autowired
