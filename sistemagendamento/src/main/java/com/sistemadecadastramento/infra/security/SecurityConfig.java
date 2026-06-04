d63c34bd3a7ff1f8a2c8446ef81eaedbfec81e31
tonin-do-java <ant.fds.com@gmail.com>
2026-06-04 20:30:50 -0300

    feat(auth): adicionar filtro de segurança e mudar permissões

--- sistemagendamento/src/main/java/com/sistemadecadastramento/infra/security/SecurityConfig.java
+++ sistemagendamento/src/main/java/com/sistemadecadastramento/infra/security/SecurityConfig.java
@@ -3,14 +3,23 @@ package com.sistemadecadastramento.infra.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private SecurityFilter securityFilter;
+    
+    public SecurityConfig(SecurityFilter securityFilter) {
+        this.securityFilter = securityFilter;
+    }
     
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -19,10 +28,19 @@ public class SecurityConfig {
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> auth
             .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html/**", "/swagger-ui/index.html#/**").permitAll()
-            .requestMatchers(HttpMethod.POST, "/usuarios/Cadastrar").permitAll()
-            .anyRequest().authenticated()
-        ); 
+            .requestMatchers(HttpMethod.POST, "/api/usuarios/Cadastrar").permitAll()
+            .requestMatchers(HttpMethod.POST, "/login").permitAll()
+            .anyRequest().hasRole("ADMIN")
+        )
+        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class); 
 
         return http.build();
+    }
+
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception{
+        return configuration.getAuthenticationManager();
     }
 }
