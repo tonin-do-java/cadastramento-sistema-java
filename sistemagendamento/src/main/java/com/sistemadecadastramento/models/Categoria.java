b9997f449a31a8ec990415d463ae0bfc1157d9fc
tonin-do-java <ant.fds.com@gmail.com>
2026-06-13 01:28:19 -0300

    feat(model): criar uma tabela para categorizar os produtos

---
+++ sistemagendamento/src/main/java/com/sistemadecadastramento/models/Categoria.java
@@ -0,0 +1,31 @@
+package com.sistemadecadastramento.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Categoria")
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome da Categoria", nullable = false)
+    private String nome;
+
+    @Column(name = "descrição", nullable = false)
+    private String descricao;
+
+    @Column(name = "Atividade", nullable = false)
+    private boolean ativo;
+}
