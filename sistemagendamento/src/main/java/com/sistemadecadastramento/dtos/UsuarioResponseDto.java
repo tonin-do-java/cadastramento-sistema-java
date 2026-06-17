e545bdec1c3d9bf810fca674c1599b27fa1df65c
tonin-do-java <ant.fds.com@gmail.com>
2026-06-16 22:46:19 -0300

    feat(User): adicionar Role no DTO de resposta

--- sistemagendamento/src/main/java/com/sistemadecadastramento/dtos/UsuarioResponseDto.java
+++ sistemagendamento/src/main/java/com/sistemadecadastramento/dtos/UsuarioResponseDto.java
@@ -1,6 +1,7 @@
 package com.sistemadecadastramento.dtos;
 
 
+import com.sistemadecadastramento.models.Roles;
 import com.sistemadecadastramento.models.Usuario;
 
 import lombok.Getter;
@@ -10,10 +11,12 @@ public class UsuarioResponseDto{
     private Long id;
     private String nome;
     private String email;
+    private Roles role;
 
     public UsuarioResponseDto(Usuario usuarioEntity){
         this.id = usuarioEntity.getId();
         this.nome = usuarioEntity.getNome();
         this.email = usuarioEntity.getEmail();
+        this.role = usuarioEntity.getRole();
     }
 }
