ff53e45ff26c6b75a87b2dc0440324e4ac4cefd0
tonin-do-java <ant.fds.com@gmail.com>
2026-08-16 17:06:43 -0300

    feat(front): main do estilo da pagina e da indexação

---
+++ frontend/src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+
+import Login from './pages/Home/Login';
+import Cadastro from './pages/Home/Cadastro';
+import Dashboard from './pages/Dashboard/Dashboard';
+import ProdutoManager from './pages/Produto/ProdutoManager'; 
+import CategoriaManager from './pages/Categoria/CategoriaManager'; 
+import MovimentacaoManager from './pages/Movimentacao/MovimentacaoManager';
+import UsuarioManager from './pages/Usuario/UsuarioManager'
+import Perfil from './pages/Perfil/Perfil'
+import Layout from './pages/Layout/Layout'
+import PreferenciaManager from './pages/Preferencias/PreferenciasManager';
+import AlteraSenhaManager from './pages/AlteraSenha/AlteraSenhaManager';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/produtos" element={<ProdutoManager />} />
+          <Route path="/categorias" element={<CategoriaManager />} />
+          <Route path="/movimentacoes" element={<MovimentacaoManager />} />
+          <Route path="/usuario" element={<UsuarioManager />} />
+          <Route path="/perfil" element={<Layout><Perfil /></Layout>} />
+          <Route path="/preferencias" element={<Layout><PreferenciaManager /></Layout>} />
+          <Route path="/alteraSenha" element={<Layout><AlteraSenhaManager /></Layout>} />
+
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
