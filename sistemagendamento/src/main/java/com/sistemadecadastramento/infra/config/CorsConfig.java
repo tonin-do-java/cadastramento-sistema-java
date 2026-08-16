a08cad0a3d9a33f1c94925079cfb7b23fdb594fd
tonin-do-java <ant.fds.com@gmail.com>
2026-08-16 02:34:50 -0300

    feat(CORS): liberar portas para o front ter acesso ao back

---
+++ sistemagendamento/src/main/java/com/sistemadecadastramento/infra/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.sistemadecadastramento.infra.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer{
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                    "http://localhost:3000", 
+                    "http://localhost:5173", 
+                    "http://localhost:4200"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+    
+}
